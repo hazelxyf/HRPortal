43fdbe36c3af00dfeca9d02520541cc094c5967d
hazelxyf <48450526+hazelxyf@users.noreply.github.com>
2019-07-08 13:28:49 +0800

    Add files via upload

---
+++ EmployeeLogicTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmployeeLogicTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EmployeeLogicTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("af4cae6b-e7dc-41aa-9ae4-d9369a70b91a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EmployeeLogicTest/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HRPortal.Models;
+
+namespace EmployeeLogicTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SalaryCalculationTest()
+        {
+            //Arrange
+            Employee testEmployee = new Employee();
+            decimal testBasicPay = 1000;
+            decimal testDeduction = 500;
+            double ExpectedSalary = 500;
+
+            //Act
+            double CalculatedSalary = testEmployee.calculateSalary(testBasicPay, testDeduction);
+
+            //Assert
+            Assert.AreEqual(ExpectedSalary, CalculatedSalary);
+        }
+    }
+}
